9a792a838cbfe88c12accb059c95fa9e87e180ed
FilipZapletal <f.zapletal0seznam.cz>
2025-08-10 18:36:32 +0200

    first init (when adding favorite page)

---
+++ frontendmovie/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+
+
+function Favorite() {
+  return <div className="favorties-empty">
+    <h2>No Favortie Movies Yet</h2>
+    <p>Start adding movies to your favorites and they will apear here</p>
+  </div>
+}
+
+export default Favorite
